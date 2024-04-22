1deebf93c037f0155acca7b0609dd4170b17db29
r.makkhmudov <r.makkhmudov@admitad.com>
2024-04-22 11:11:56 +0300

    edit name of dir

--- backend/core/src/realm/urls.py
+++ backend/core/src/realm/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .view import CreateServerView
+from .views import CreateServerView
 
 urlpatterns = [
     path("create/", CreateServerView.as_view(), name="create"),
