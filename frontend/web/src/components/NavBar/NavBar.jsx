19675d5c2a47e1566e3caa098d984113f70f29fb
nnnLik <rslmakhmudov@gmail.com>
2024-05-10 18:00:54 +0300

    add navbar

---
+++ frontend/web/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div>
+      kiss
+    </div>
+  );
+};
+
+export default NavBar;
--- frontend/web/src/routes/PrivateRoute.jsx
+++ frontend/web/src/routes/PrivateRoute.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import {Context} from "../index";
 import {useContext} from "react";
+import NavBar from "../components/NavBar/NavBar";
 
 const PrivateRoute = () => {
   const {store} = useContext(Context)
   const location = useLocation();
-  console.log(store.isAuth);
+
   return store.isAuth === true ? (
     <>
+      <NavBar/>
       <Outlet />
     </>
   ) : (
-    <Navigate to="/" state={{ from: location }} replace />
+    <>
+      <Navigate to="/" state={{ from: location }} replace />
+    </>
   );
 };
 
